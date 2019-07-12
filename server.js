fa28bcd7983e6155063560369973118654c9e09e
Vasily93 <markovavasily@gmail.com>
2019-07-12 06:20:03 -0700

    functions and tests  for have item, write item, update item

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./lib/app');
 
-const PORT  = process.env.PORT || 1993;
-
-app.listen(PORT, () => {
-  console.log(`started on ${PORT}`);
+app.listen(7890, () => {
+  console.log('Started');
 });
+
+
--- test/item.test.js
+++ test/item.test.js
@@ -4,7 +4,7 @@ const app = require('../lib/app');
 describe('items routes', () => {
   it('can create item with POST', () => {
     return request(app)
-      .post('api/v1/items')
+      .post('/api/v1/items')
       .send({ fruit:'banana', color:'yellow', amount:3, price:'$30' })
       .then(res => {
         expect(res.body).toEqual({
@@ -15,4 +15,51 @@ describe('items routes', () => {
         });
       });
   });
+  it ('can get an array of items if GET', () => {
+    return request(app)
+      .get('/api/v1/items')
+      .then(res => {
+        expect(res.body).toEqual([{
+          fruit:'banana',
+          color:'yellow',
+          amount:3,
+          price:'$30'
+        }]);
+      });
+  });
+
+  it('it can get item by index', () => {
+    return request(app)
+      .get('/api/v1/items/0')
+      .then(res => {
+        expect(res.body).toEqual({
+          fruit:'banana',
+          color:'yellow',
+          amount:3,
+          price:'$30'
+        });
+      });
+  });
+
+  it('updetes item with PUT', () => {
+    const newItem = {
+      fruit:'shmanana',
+      color:'shmyellow',
+      amount:300,
+      price:'$3000'
+    };
+
+    return request(app)
+      .put('/api/v1/items/0')
+      .send(newItem)
+      .then(res => {
+        expect (res.body).toEqual({
+          fruit:'shmanana',
+          color:'shmyellow',
+          amount:300,
+          price:'$3000'
+        });
+      });
+  });
 });
+
